d2fc2675b0e445bce4c7adab35f682bb3a911532
Claudia <claui@users.noreply.github.com>
2018-09-17 14:02:59 +0200

    gog-stories-untold: adapt to custom libs

--- Casks/gog-stories-untold.rb
+++ Casks/gog-stories-untold.rb
@@ -2,29 +2,29 @@ cask 'gog-stories-untold' do
   version '1.3'
   sha256 'c48f5d14192e4e5cb2b92c0a540ad300d71376d3faf65a50d0300dae889b1900'
 
-  unless (staged_path / 'lib').exist?
-    FileUtils.mkdir_p(staged_path)
-    system '/bin/cp', '-R', '--',
-           cask.sourcefile_path.parent.parent / 'lib', staged_path
-  end
-  require staged_path / 'lib/utils.rb'
+  require 'utils/lgog_downloader'
 
   # nil was verified as official when first introduced to the cask
   url do
-    Utils::LGOGDownloader.installer_url 'stories_untold',
-                                        'en2installer0'
+    installer = Utils::LGOGDownloader
+                .game('stories_untold')
+                .installer('en2installer0')
+    Utils::LGOGDownloader.url(installer)
   end
   name 'Stories Untold'
   homepage 'https://www.gog.com/game/stories_untold'
 
   depends_on formula: 'lgogdownloader'
-  container type: :naked
+  container type: :pkg
 
   app 'Stories Untold.app'
 
   preflight do
-    Utils::LGOGDownloader.rename_artifact! self, 'stories_untold',
-                                           'en2installer0'
+    installer = Utils::LGOGDownloader
+                .game('stories_untold')
+                .installer('en2installer0')
+    Utils::LGOGDownloader.rename_artifact! self, installer
+
     system_command '/usr/sbin/pkgutil',
                    args: [
                            '--expand',
