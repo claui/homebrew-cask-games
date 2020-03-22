71ca509cc128738d5289404c4b9c58d0cd5053a8
Claudia <claui@users.noreply.github.com>
2020-03-22 14:17:31 +0100

    lgog_downloader: add debug statements

--- lib/utils/lgog_downloader.rb
+++ lib/utils/lgog_downloader.rb
@@ -50,6 +50,7 @@ module Cask
 
       def self.rename_artifact!(cask, installer)
         target_name = installer.path.split('/').last
+        odebug "Moving #{installer.id} to #{target_name}"
 
         args = [
                  '--',
